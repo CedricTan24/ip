2647081994dd6d7924f5f118f31188df73915d72
CedricTan24 <e1384331@u.nus.edu>
2026-01-28 14:53:45 +0800

    Level-2: add tasks and list tasks

--- src/main/java/CedricBot.java
+++ src/main/java/CedricBot.java
@@ -1,9 +1,13 @@
 import java.util.Scanner;
 
 public class CedricBot {
+    private static final int MAX_TASKS = 100;
     private static final String LINE = "____________________________________________________________";
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        String[] tasks = new String[MAX_TASKS];
+        int taskCount = 0;
 
         System.out.println(LINE);
         System.out.println("Hello! I'm CedricBot");
@@ -19,6 +23,28 @@ public class CedricBot {
                 System.out.println("CedricBot, Signing Out! ♥");
                 System.out.println(LINE);
                 break;
+            }
+
+            if (input.equals("list")) {
+                System.out.println(LINE);
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(LINE);
+                continue;
+            }
+
+            if (taskCount < MAX_TASKS) {
+                tasks[taskCount] = input;
+                taskCount++;
+
+                System.out.println(LINE);
+                System.out.println("added: " + input);
+                System.out.println(LINE);
+            } else {
+                System.out.println(LINE);
+                System.out.println("Your task list is full.");
+                System.out.println(LINE);
             }
 
             System.out.println(LINE);
